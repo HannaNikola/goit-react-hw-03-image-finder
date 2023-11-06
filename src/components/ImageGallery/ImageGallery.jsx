47f598f449b477051fad87812b4106b41fedd562
HannaNikola <h.nikolaienko32@gmail.com>
2023-11-06 21:51:24 +0100

    update

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,13 +1,13 @@
 import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem"
 
-export const ImageGallery = ({ images}) => {
+export const ImageGallery = ({ images }) => {
     return (
         <ul className="gallery">
             {images.map((image) => (
-                
+
                 <ImageGalleryItem key={image.id} image={image} />
-              ))  
-            } 
+            ))
+            }
         </ul>
     )
 }
